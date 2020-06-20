208c6c211153eefe32a87cce9f3d8fd568221a57
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-06-20 09:47:02 +0300

    Start doing the social news, create an array object

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-// write your code here
-console.log('it works!');
+const linkList = [
+    {
+        title: "The best Game",
+        url: "http://the_best_game.com",
+        author: "Jackson"
+    },
+    {
+        title: "Traveling around the world",
+        url: "https://travel/around_the_world.com",
+        author: "David"
+    },
+    {
+        title: "Winning a game",
+        url: "https://winning_game.com",
+        author: "James"
+    },
+    {
+        title: "Surprising World",
+        url: "http://surprising_world.com",
+        author: "Catherine"
+    }
+];
+
